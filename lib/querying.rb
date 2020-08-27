8e73b5a6e097a6fff375c3b5ac2d29da287b029d
Christie Yoon <cleex1999@gmail.com>
2020-08-27 02:01:10 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -21,5 +21,5 @@ def select_series_title_with_most_human_characters
 end
 
 def select_character_names_and_number_of_books_they_are_in
-  "SELECT characters.name, COUNT(character_books) FROM characters_books JOIN characters ON character_books.character_id = characters.id GROUP BY characters.names;"
+  "SELECT characters.name, COUNT(character_books) FROM character_books JOIN characters ON character_books.character_id = characters.id GROUP BY characters.names;"
 end
