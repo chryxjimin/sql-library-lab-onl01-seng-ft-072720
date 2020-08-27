485bbb0d56f8a801eaaaba92b960e315717d1205
Christie Yoon <cleex1999@gmail.com>
2020-08-27 02:32:38 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -32,7 +32,7 @@ def select_series_title_with_most_human_characters
    JOIN books
    ON books.series_id = series.id
    JOIN character_books.book_id = books.id
-   WHERE characters.species = 'human'
+   WHERE characters.species = "human"
    ORDER BY COUNT(characters.species) DESC LIMIT 1;"
  end
 
