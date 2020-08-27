88652abba56e6a787684239b33dc9100292c2b73
Christie Yoon <cleex1999@gmail.com>
2020-08-27 02:13:05 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -27,7 +27,7 @@ def select_name_and_series_subgenres_of_authors
 end
 
 def select_series_title_with_most_human_characters
-  "SELECT Series.title, SUM(character_books.character_id)
+  "SELECT series.title, SUM(character_books.character_id)
   FROM series
   JOIN characters ON series.author_id = characters.author_id
   GROUP BY species
