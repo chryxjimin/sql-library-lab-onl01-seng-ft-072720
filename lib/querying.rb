9481ddc8d1199a06c42225b717e8a7b8e1c49160
Christie Yoon <cleex1999@gmail.com>
2020-08-27 02:38:35 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -31,7 +31,7 @@ def select_series_title_with_most_human_characters
    FROM series
    JOIN books
    ON books.series_id = series.id
-   JOIN characters_books
+   JOIN character_books
    ON character_books.book_id = books.id
    JOIN characters
    ON character_books.character_id = characters.id
